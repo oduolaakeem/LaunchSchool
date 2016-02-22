58aacb7988f85fce25717a7c17a3895e175bb63b
Lucas Ochoa <lucasou89@gmail.com>
2016-02-22 02:38:42 +0000

    Finished deploying app to heroku

--- Programming_and_Back-end_Development/170_Web_Development/004_working_with_sinatra/012_code_challenge_users_and_interests/users_and_interests/users_and_interests.rb
+++ Programming_and_Back-end_Development/170_Web_Development/004_working_with_sinatra/012_code_challenge_users_and_interests/users_and_interests/users_and_interests.rb
@@ -2,9 +2,40 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 require 'yaml'
+require 'pry'
 
-USERS_INFO = YAML.load_file("data/users.yml")
+# USERS_INFO = nil
+
+before do
+  USERS_INFO = YAML.load_file("data/users.yml")
+  @users = USERS_INFO.keys.map! { |user| user.to_s }
+end
+
+helpers do
+  def number_of_users
+    @users.count.to_s + " users"
+  end
+  
+  def number_of_interests
+    total = 0
+    @users.each do |user|
+      total += USERS_INFO[user.to_sym][:interests].count
+    end
+    total.to_s + " interests"
+  end
+end
 
 get "/" do
-  USERS_INFO.to_s
+  @title = "Welcome"
+  erb :home
 end
+
+get "/users/:user" do
+  @user = params[:user]
+  @title = @user.capitalize
+  @email = USERS_INFO[@user.to_sym][:email]
+  @interests = USERS_INFO[@user.to_sym][:interests]
+
+  erb :user
+end
+
---
+++ Programming_and_Back-end_Development/170_Web_Development/005_deploying_sinatra_applications/002-003_configuring_and_deploying_an_application_for_deployment/book_viewer/book_viewer.rb
@@ -0,0 +1,79 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "tilt/erubis"
+
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+helpers do
+  def in_paragraphs(text)
+    paragraph_number = 0
+    text.split("\n\n").map do |line|
+      paragraph_number += 1
+      "<p id=\"#{paragraph_number}\">#{line}</p>"
+    end.join
+  end
+  
+  def in_paragraphs_without_p_tag(text)
+    paragraphs_hash = {}
+    paragraph_number = 0
+    text.split("\n\n").each do |line|
+      paragraph_number += 1
+      paragraphs_hash[paragraph_number] = line
+    end
+    paragraphs_hash
+  end
+  
+  def highlight_result(text)
+    text.gsub(params[:query], "<strong>#{params[:query]}</strong>")
+  end
+end
+
+not_found do
+  redirect "/"
+end
+
+get "/" do
+  # File.read "public/template.html"
+  @title = "The Adventures of Sherlock Holmes"
+  erb :home
+end
+
+get "/chapters/:number" do
+  @chapter = File.read("data/chp#{params[:number]}.txt")
+  
+  number = params[:number].to_i
+  chapter_name = @contents[number - 1]
+  @title = "Chapter #{number}: #{chapter_name}"
+  
+  erb :chapter
+end
+
+get "/search" do
+  @chapters_list = {
+    :chapter_number => [],
+    :chapter_title => [],
+    :paragraph_number => [],
+    :paragraph_text => []
+  }
+  if params[:query]
+    @contents.each_with_index do |chapter_title, index|
+      chapter_paragraphs = File.read("data/chp#{index + 1}.txt")
+      chapter_paragraphs = in_paragraphs_without_p_tag(chapter_paragraphs)
+      chapter_paragraphs.each_pair do |paragraph, text|
+        if text.include?(params[:query])
+          @chapters_list[:chapter_number] << index + 1
+          @chapters_list[:chapter_title] << chapter_title
+          @chapters_list[:paragraph_number] << paragraph
+          @chapters_list[:paragraph_text] << highlight_result(text)
+        end
+      end
+    end
+  end
+  erb :search
+end
+
+get "/test" do
+  request.path_info
+end
